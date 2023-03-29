eefc7270b5627cbb4237b13b348fdc8159729737
Lucas Delanier <delanierlucas@gmail.com>
2023-03-29 14:59:47 +0200

    delete useless reducers

--- redux/reducers/appReducer.ts
+++ redux/reducers/appReducer.ts
@@ -1,4 +1,4 @@
-import {POP_FIRST_TRENDING, FETCH_TRENDING_MOVIE, FETCH_TRENDING_ID, ADD_WATCHLATER, FETCH_WATCHLATER, ADD_FAVOURITE, FETCH_FAVOURITE, LOAD_WATCHLATER, LOAD_FAVOURITE} from "../constants";
+import {POP_FIRST_TRENDING, FETCH_TRENDING_MOVIE, ADD_WATCHLATER, ADD_FAVOURITE, LOAD_WATCHLATER, LOAD_FAVOURITE} from "../constants";
 import Movie from "../../model/Movie";
 
 const initialState = {
@@ -14,15 +14,6 @@ export default (state = initialState, action) => {
             // @ts-ignore
             return {...state, watchLaterMovies: action.payload};
         case LOAD_FAVOURITE:
-            // @ts-ignore
-            return {...state, favouriteMovies: action.payload};
-        case FETCH_TRENDING_ID:
-            // @ts-ignore
-            return {...state, trendingIDs: action.payload};
-        case FETCH_WATCHLATER:
-            // @ts-ignore
-            return {...state, watchLaterMovies: action.payload};
-        case FETCH_FAVOURITE:
             // @ts-ignore
             return {...state, favouriteMovies: action.payload};
         case FETCH_TRENDING_MOVIE:
